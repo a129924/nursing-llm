f593b32ee6d3cf85dcced736b04a17e7bd44242c
andrew <a129924@gmail.com>
2025-09-30 14:08:20 +0800

    feat: 更新 PydanticValidator 的驗證方法
    - 將 validate 方法中的 model_validate 替換為 model_validate_json，以支持 JSON 格式的驗證。

--- server/src/nursing_llm_server/applications/validators/pydantic/pydantic_validator.py
+++ server/src/nursing_llm_server/applications/validators/pydantic/pydantic_validator.py
@@ -8,4 +8,4 @@ class PydanticValidator(PydanticModelValidatorABC):
     @override
     @classmethod
     def validate(cls, validate_schema: type[BaseModel], raw: str) -> BaseModel:
-        return validate_schema.model_validate(raw)
+        return validate_schema.model_validate_json(raw)
